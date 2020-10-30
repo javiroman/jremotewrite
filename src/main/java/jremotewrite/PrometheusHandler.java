883e53f3a2f473ec5974d293d71b39a7419166e0
Javi Roman <jroman.espinar@gmail.com>
2020-10-30 08:41:01 +0100

    added NiFi checkstyle rules

--- src/main/java/jremotewrite/PrometheusHandler.java
+++ src/main/java/jremotewrite/PrometheusHandler.java
@@ -22,7 +22,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
