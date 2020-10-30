f4ce5af27bf45f07efcc2790002b755534ee4d91
Javi Roman <jroman.espinar@gmail.com>
2020-10-30 13:54:16 +0100

    enabled JUnit

---
+++ src/test/java/jremotewrite/TestPrometheusHandler.java
@@ -0,0 +1,12 @@
+package test.java.jremotewrite;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestPrometheusHandler {
+    @Test
+    public void test() {
+        Assert.fail("Not yet implemented");
+    }
+}
