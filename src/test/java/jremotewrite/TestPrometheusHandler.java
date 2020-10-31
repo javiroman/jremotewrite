5998989f35c09be6f6f5c420cf6cc1a451f01c6a
Javi Roman <jroman.espinar@gmail.com>
2020-10-31 10:02:10 +0100

    minimal structure for tests

--- src/test/java/jremotewrite/TestPrometheusHandler.java
+++ src/test/java/jremotewrite/TestPrometheusHandler.java
@@ -1,12 +1,23 @@
 package test.java.jremotewrite;
 
-
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestPrometheusHandler {
+    @Before
+    public void InitServer() {
+
+    }
+
     @Test
     public void test() {
         Assert.fail("Not yet implemented");
+    }
+
+    @After
+    public void ShutdownServer() {
+
     }
 }
